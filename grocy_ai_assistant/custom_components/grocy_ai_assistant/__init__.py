a99e7728b13c461ac37d9858865c493ddd58816c
Benjamin Graf <graf.benjamin91@gmail.com>
2026-03-08 22:55:21 +0100

    a

--- grocy_ai_assistant/custom_components/grocy_ai_assistant/__init__.py
+++ grocy_ai_assistant/custom_components/grocy_ai_assistant/__init__.py
@@ -94,6 +94,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # Listener & Plattformen
     entry.async_on_unload(entry.add_update_listener(update_listener))
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "text"])
+    
+    hass.states.async_set(
+        f"sensor.{DOMAIN}_response", 
+        "System bereit", 
+        {"friendly_name": "Grocy AI Response"}
+    )
 
     return True
 
