da8bc5e4003e268b9f65e19511570d6217b45aa0
Benjamin Graf <graf.benjamin91@gmail.com>
2026-03-08 19:59:36 +0100

    options

--- grocy_ai_assistant/custom_components/grocy_ai_assistant/config_flow.py
+++ grocy_ai_assistant/custom_components/grocy_ai_assistant/config_flow.py
@@ -1,25 +1,31 @@
-
-import voluptuous as vol
 from homeassistant import config_entries
+import voluptuous as vol
 
-DOMAIN = "grocy_ai_assistant"
+# ... dein bestehender ConfigFlow ...
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class GrocyAIConfigFlow(config_entries.ConfigFlow, domain="grocy_ai_assistant"):
+    # ... dein async_step_user ...
 
-    async def async_step_user(self, user_input=None):
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        return GrocyAIOptionsFlowHandler(config_entry)
 
+class GrocyAIOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
         if user_input is not None:
-
-            return self.async_create_entry(
-                title="Grocy AI Assistant",
-                data=user_input
-            )
-
-        schema = vol.Schema({
-            vol.Required("api_url"): str
-        })
+            return self.async_create_entry(title="", data=user_input)
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=schema
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Required(
+                    "api_key", 
+                    default=self.config_entry.options.get(
+                        "api_key", self.config_entry.data.get("api_key")
+                    )
+                ): str,
+            }),
         )
